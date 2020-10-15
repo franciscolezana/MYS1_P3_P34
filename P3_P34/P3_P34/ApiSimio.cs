f2a73d251959b7f3423b0c94b5c6741e9a8df110
Francisco Lezana <fraaanciscolezana8@gmail.com>
2020-10-15 16:34:28 -0600

    ADD: ejemplos de objetos y conexiones

--- P3_P34/P3_P34/ApiSimio.cs
+++ P3_P34/P3_P34/ApiSimio.cs
@@ -20,6 +20,7 @@ namespace P3_P34
         private string[] warnings;
         private IModel model;
         private IIntelligentObjects intelligentObjects;
+        int ContadorPath = 1, ContadorServer = 1, ContadorSource = 1, ContadorSink = 1, ContadorPathSimple = 1, ContadorTimepath = 1, ContadorConveyor = 1, ContadorSeparator = 1, ContadorCombiner = 1;
 
         public ApiSimio()
         {
@@ -31,13 +32,75 @@ namespace P3_P34
 
         public void crearModelo()
         {
-            //Source
-            intelligentObjects.CreateObject("Source", new FacilityLocation(0, 0, 5)); //x,z,y
-            intelligentObjects.CreateObject("Source", new FacilityLocation(0, 0, 10)); //x,z,y
-            intelligentObjects.CreateObject("Source", new FacilityLocation(0, 0, 15)); //x,z,y
-            model.Facility.IntelligentObjects["Source1"].Properties["InterarrivalTime"].Value = "Random.Exponential(5)";
-            SimioProjectFactory.SaveProject(proyectoApi, rutafinal, out warnings);
+
+            try
+            {
+                //Source
+                intelligentObjects.CreateObject("Source", new FacilityLocation(5, 0, -5)); //x,z,y
+                model.Facility.IntelligentObjects["Source" + ContadorSource].Properties["InterarrivalTime"].Value = "0.1";
+
+                //Server
+                intelligentObjects.CreateObject("Server", new FacilityLocation(10, 0, -5)); //x,z,y
+                model.Facility.IntelligentObjects["Server" + ContadorServer].Properties["ProcessingTime"].Value = "2";
+
+                //Sink
+                intelligentObjects.CreateObject("Sink", new FacilityLocation(15, 0, -5)); //x,z,y
+
+                //Source a Server
+                intelligentObjects.CreateLink("Path", ((IFixedObject)model.Facility.IntelligentObjects["Source1"]).Nodes[0], ((IFixedObject)model.Facility.IntelligentObjects["Server1"]).Nodes[0], null);
+
+
+                //Server a Sink
+                intelligentObjects.CreateLink("Path", ((IFixedObject)model.Facility.IntelligentObjects["Server1"]).Nodes[1], ((IFixedObject)model.Facility.IntelligentObjects["Sink1"]).Nodes[0], null);
+
+                //Cambiar propiedades de path
+                model.Facility.IntelligentObjects["Path" + ContadorPathSimple].Properties["DrawnToScale"].Value = "False";
+                model.Facility.IntelligentObjects["Path" + ContadorPathSimple].Properties["LogicalLength"].Value = "0";
+
+                ContadorPathSimple++;
+                ContadorServer++;
+                ContadorSink++;
+                ContadorSource++;
+
+                //Combiners y Separators
+                intelligentObjects.CreateObject("Source", new FacilityLocation(5, 0, -20)); //x,z,y
+                intelligentObjects.CreateObject("Sink", new FacilityLocation(30, 0, -20)); //x,z,y
+
+                //combiner
+                intelligentObjects.CreateObject("Combiner", new FacilityLocation(10, 0, -20)); //x,z,y
+
+                model.Facility.IntelligentObjects["Combiner" + ContadorCombiner].Properties["FailureType"].Value = "Processing Count Based";
+                model.Facility.IntelligentObjects["Combiner" + ContadorCombiner].Properties["CountBetweenFailures"].Value = "10";
+                model.Facility.IntelligentObjects["Combiner" + ContadorCombiner].Properties["TimeToRepair"].Value = "1";
+                model.Facility.IntelligentObjects["Combiner" + ContadorCombiner].Properties["InitialCapacity"].Value = "5";
+                model.Facility.IntelligentObjects["Combiner" + ContadorCombiner].Properties["BatchQuantity"].Value = "10";
+                model.Facility.IntelligentObjects["Combiner" + ContadorCombiner].Properties["ProcessingTime"].Value = "2";
+
+                //separator
+                intelligentObjects.CreateObject("Separator", new FacilityLocation(20, 0, -20)); //x,z,y
+
+                //Source a Combiner
+                intelligentObjects.CreateLink("Path", ((IFixedObject)model.Facility.IntelligentObjects["Source1"]).Nodes[0], ((IFixedObject)model.Facility.IntelligentObjects["Combiner1"]).Nodes[0], null);
+                intelligentObjects.CreateLink("Path", ((IFixedObject)model.Facility.IntelligentObjects["Source2"]).Nodes[0], ((IFixedObject)model.Facility.IntelligentObjects["Combiner1"]).Nodes[1], null);
+
+                //Combiner a Separator
+                intelligentObjects.CreateLink("Path", ((IFixedObject)model.Facility.IntelligentObjects["Combiner" + ContadorCombiner]).Nodes[2], ((IFixedObject)model.Facility.IntelligentObjects["Separator" + ContadorSeparator]).Nodes[0], null);
+
+                //Separator a Sink
+                intelligentObjects.CreateLink("Path", ((IFixedObject)model.Facility.IntelligentObjects["Separator" + ContadorSeparator]).Nodes[1], ((IFixedObject)model.Facility.IntelligentObjects["Sink2"]).Nodes[0], null);
+                intelligentObjects.CreateLink("Path", ((IFixedObject)model.Facility.IntelligentObjects["Separator" + ContadorSeparator]).Nodes[2], ((IFixedObject)model.Facility.IntelligentObjects["Sink2"]).Nodes[0], null);
+                ContadorCombiner++;
+                ContadorSeparator++;
+
+
+                //CREACION MODELO
+                SimioProjectFactory.SaveProject(proyectoApi, rutafinal, out warnings);
+                Console.WriteLine("Modelo Creado");
+            }
+            catch
+            {
+                Console.WriteLine("Error al crear modelo");
+            }
         }
     }
-
 }
